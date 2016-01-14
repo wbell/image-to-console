6365fea8e3c371d25a1aecc8bf7d217a0c7661fa
Willie Bell <willie.bell@us.pwc.com>
2016-01-14 17:39:05 -0500

    improve docs, complete collect and buffer methods

--- src/image-to-console.js
+++ src/image-to-console.js
@@ -9,25 +9,33 @@ var mkdirp = require('mkdirp');
 var Logger = require('./logger');
 var log = null;
 var q = require('q');
+var jimp = require('jimp');
+var readimage = require('readimage');
 
 /**
- * ImageToConsole class function
+ * ImageToConsole constructor function
  * @param {Array} imgPaths  array of image paths
  * @param {Object} options  options map passed to plugin
  * @return {Promise}
  */
 function ImageToConsole(imgPaths, options) {
-  if(!this instanceof ImageToConsole){
-    return new ImageToConsole(imgPaths, options, defaults);
+  if (!(this instanceof ImageToConsole)) {
+    return new ImageToConsole(imgPaths, options);
   }
 
+  // set properties
   this.paths = imgPaths;
   this.options = _.extend({}, defaults, options);
+  this.queue = [];
 
+  // create logger with options from user
   log = new Logger(this.options);
 
+  log.info('ImageToConsole instance constructed');
+
   return this.collectImages()
-    .then(this.transformImages);
+    .then(this.bufferImages)
+    .then(this.resizeImages);
 }
 
 /**
@@ -43,51 +51,51 @@ ImageToConsole.prototype.collectImages = function CollectImages() {
 
   // create temp directory
   mkdirp.sync(options.temp);
+  log.info('temp directory created');
 
-  _.forEach(paths, function(path, i) {
+  _.forEach(paths, function(filepath, i) {
     var requestOptions = null;
-    var filename = _.last(path.split('/'));
+    var filename = _.last(filepath.split('/'));
     var localPath = path.resolve(options.temp, filename);
+    var ws = fs.createWriteStream(localPath)
+      .on('error', _writeError)
+      .on('finish', function(err) {
+        _writeSuccess(err, localPath, i);
+      });
 
-    /**
-     * if web image, use request to GET
-     * if local image, pipe to temp
-     */
-    if (_.startsWith(path.toLowerCase(), 'http')) {
-      requestOptions = {
-        url: path,
-        agentOptions: {
-          secureProtocol: 'SSLv3_method'
-        }
-      };
+    // if web image, request over http
+    // if local, pipe to temp location
+    if (_.startsWith(filepath.toLowerCase(), 'http')) {
 
-      request(requestOptions)
-        .pipe(fs.createWriteStream(localPath))
-        .on('finish', function(err){
-          _writeSuccess(err, localPath, i);
-        })
-        .on('error', _writeError);
+      log.info('requesting "' + filename + '" over http');
+
+      request(filepath)
+        .pipe(ws)
+        .on('error', _readError);
 
     } else {
-      fs.createReadStream(path)
-        .pipe(fs.createWriteStream(localPath))
-        .on('finish', function(err) {
-          _writeSuccess(err, localPath, i);
-        })
-        .on('error', _writeError);
+
+      log.info('writing "' + filename + '" to temp directory');
+
+      fs.createReadStream(filepath)
+        .pipe(ws)
+        .on('error', _readError);
     }
   });
 
   /**
    * callback for successful write
    * @param  {Object} err error object
+   * @param {String} localPath image path from temp dir
+   * @param {Number} i index of image
    */
-  function _writeSuccess(err) {
+  function _writeSuccess(err, localPath, i) {
     if (err) {
       log.error(err);
       deferred.reject(err);
     }
 
+    log.info('"' + localPath + '" saved');
     localPaths[i] = localPath;
     _checkLengths();
   }
@@ -98,6 +106,7 @@ ImageToConsole.prototype.collectImages = function CollectImages() {
   function _checkLengths() {
     if (_.compact(localPaths).length === paths.length) {
       deferred.resolve(localPaths);
+      log.success('all images saved to temp directory');
     }
   }
 
@@ -106,7 +115,16 @@ ImageToConsole.prototype.collectImages = function CollectImages() {
    * @param  {Object} err error object
    */
   function _writeError(err) {
-    log.error(err);
+    log.error('Write error: ' + JSON.stringify(err));
+    deferred.reject(err);
+  }
+
+  /**
+   * callback for read error
+   * @param  {Object} err error object
+   */
+  function _readError(err) {
+    log.error('Read error: ' + JSON.stringify(err));
     deferred.reject(err);
   }
 
@@ -114,14 +132,96 @@ ImageToConsole.prototype.collectImages = function CollectImages() {
 };
 
 /**
- * resizes images to correct dimensions, saves as png
+ * converts all images and frames into buffers rgba buffers
  * @param  {Array} localPaths list of locally saved image paths
- * @return {Promise}
+ * @return {Promise}          resolves with array of image buffers
  */
-ImageToConsole.prototype.transformImages = function TransformImages(localPaths){
-  log.log(localPaths);
-  log.warn(localPaths);
-  log.error(localPaths);
+ImageToConsole.prototype.bufferImages = function BufferImages(localPaths) {
+
+  var deferred = q.defer();
+  var bufferPrep = [];
+
+  _.forEach(localPaths, function(localPath, i) {
+
+    // get file buffer to pass to "readimage"
+    fs.readFile(localPath, function(err, imageBuffer) {
+
+      if (err) {
+        log.error('file read error: ' + JSON.stringify(err));
+        deferred.reject(err);
+      }
+
+      log.info('file read success: '+localPath);
+      _readFileCallback(imageBuffer, localPath, i);
+    });
+
+  });
+
+  /**
+   * called upon 'readFile' success
+   * @param  {Buffer} imageBuffer image buffer
+   * @param  {String} localPath   temp path
+   * @param  {Number} i           index of image
+   */
+  function _readFileCallback(imageBuffer, localPath, i) {
+
+    // get data from image, pass to parsing method
+    readimage(imageBuffer, function(err, imageData) {
+
+      if (err) {
+        log.error('"readimage" error: ' + JSON.stringify(err));
+        deferred.reject(err);
+      }
+
+      log.info('"readimage" success: '+localPath);
+      _readImageCallback(imageData, localPath, i);
+    });
+  }
+
+  /**
+   * called upon 'readimage' success
+   * @param  {Object} imageData hash of image metadata
+   * @param  {String} localPath   temp path
+   * @param  {Number} i           index of image
+   */
+  function _readImageCallback(imageData, localPath, i) {
+    console.log(imageData);
+    var frames = imageData.frames;
+    var myBuffers = [];
+    var frame = frames[0];
+
+    if(frame.delay) {
+      log.info('native animation speed is "'+frame.delay+'" for "'+localPath+'"');
+    }
+
+    // if animated gif, there will be multiple frames
+    if(frames.length > 1){
+      _.forEach(frames, function(frame){
+        myBuffers.push(frame.data);
+      });
+    } else {
+      myBuffers.push(frame.data);
+    }
+
+    bufferPrep[i] = myBuffers;
+
+    if(_.compact(bufferPrep).length === localPaths.length){
+      deferred.resolve(_.flatten(bufferPrep));
+    }
+
+  }
+
+  return deferred.promise;
+
+};
+
+/**
+ * resizes image buffers to the specified dimensions
+ * @param  {Array} bufferArr array of image buffers
+ * @return {Promise}         resolves with array of resized buffers
+ */
+ImageToConsole.prototype.resizeImages = function ResizeImages(bufferArr){
+  console.log(bufferArr);
 };
 
 module.exports = ImageToConsole;
--- src/logger.js
+++ src/logger.js
@@ -2,27 +2,32 @@
 
 var chalk = require('chalk');
 var prefix = '[itc]';
-var separator = '';
 
 function Logger(options){
   this.logLevel = options.log;
 }
 
-Logger.prototype.log = function Log(msg) {
+Logger.prototype.info = function Log(msg) {
   if(this.logLevel < 2) return false;
 
-  console.log(chalk.cyan(prefix), chalk.white(separator), chalk.gray(JSON.stringify(msg)));
+  console.log(chalk.cyan(prefix), chalk.gray(JSON.stringify(msg)));
+};
+
+Logger.prototype.success = function Success(msg) {
+  if(this.logLevel < 1) return false;
+
+  console.log(chalk.green(prefix), chalk.gray(JSON.stringify(msg)));
 };
 
 Logger.prototype.warn = function Warn(msg) {
   if(this.logLevel < 1) return false;
 
-  console.log(chalk.yellow(prefix), chalk.white(separator), chalk.gray(JSON.stringify(msg)));
+  console.log(chalk.yellow(prefix), chalk.gray(JSON.stringify(msg)));
 };
 
 Logger.prototype.error = function Error(msg) {
 
-  console.log(chalk.red(prefix), chalk.white(separator), chalk.gray(JSON.stringify(msg)));
+  console.log(chalk.red(prefix), chalk.gray(JSON.stringify(msg)));
 };
 
 module.exports = Logger;
